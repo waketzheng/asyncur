e4baa5096019ef9360e53fdd8d487afb0b94cd83
Waket Zheng <waketzheng@gmail.com>
2023-09-09 00:15:58 +0800

    Downgrade anyio version and add unittests for aio

--- asyncur/aio.py
+++ asyncur/aio.py
@@ -15,10 +15,22 @@ def ensure_afunc(coro: Coroutine | Callable) -> Callable:
 
 
 def run_async(coro: Coroutine | Callable):
+    """Compare with anyio.run and asyncio.run
+
+    Usage::
+        >>> async def afunc(n=1):
+        ...     return n
+        ...
+        >>> run_async(afunc)  # get the same result as: await afunc()
+        1
+        >>> run_async(afunc(2))  # get the same result as: await afunc(2)
+        2
+    """
     return anyio.run(ensure_afunc(coro))
 
 
 async def gather(*coros):
+    """Similar like asyncio.gather, but return a list"""
     results = [None] * len(coros)
 
     async def runner(coro, i):
@@ -33,6 +45,21 @@ async def gather(*coros):
 
 @asynccontextmanager
 async def start_tasks(coro: Coroutine | Callable, *more: Coroutine | Callable):
+    """Make it easy to convert asyncio.create_task
+
+    Usage:
+
+    .. code-block:: python3
+
+        async def startup():
+            # cost a long time to do sth async
+            await anyio.sleep(1000)
+
+        @contextlib.asynccontextmanager
+        async def lifespan(app):
+            async with start_tasks(startup()):
+                yield
+    """
     async with anyio.create_task_group() as tg:
         with anyio.CancelScope(shield=True):
             tg.start_soon(ensure_afunc(coro))
---
+++ tests/test_aio.py
@@ -0,0 +1,82 @@
+import functools
+from contextlib import asynccontextmanager
+from datetime import datetime
+from typing import Any
+
+import anyio
+import pytest
+
+from asyncur.aio import gather, run_async, start_tasks
+
+
+def test_run_async():
+    async def foo(sth: Any = ""):
+        return sth
+
+    assert run_async(foo) == ""
+    assert run_async(foo()) == ""
+    assert run_async(foo(1)) == 1
+    assert run_async(foo(None)) is None
+    assert run_async(foo(foo)) == foo
+    assert run_async(functools.partial(foo, 2)) == 2
+
+
+@pytest.mark.anyio
+async def test_gather():
+    async def a():
+        return 1
+
+    async def b():
+        return "2"
+
+    async def c():
+        pass
+
+    assert (await gather(a(), b(), c())) == [1, "2", None]
+
+
+@pytest.mark.anyio
+async def test_start_tasks():
+    root = anyio.Path(__file__).parent
+    names = ("tmp.txt", "tmp2.txt", "tmp3.txt")
+    async def remove_files():
+        for name in names:
+            if await (p := root / name).exists():
+                await p.unlink()
+    await remove_files()
+    async def startup():
+        await root.joinpath(names[0]).touch()
+        await anyio.sleep(1)
+        await root.joinpath(names[1]).touch()
+
+    async def running():
+        while True:
+            now = datetime.now()
+            await root.joinpath(names[2]).write_text(str(now))
+            await anyio.sleep(0.5)
+
+    @asynccontextmanager
+    async def lifespan(app):
+        now = datetime.now()
+        async with start_tasks(startup, running):
+            await anyio.sleep(0.1)
+            assert await root.joinpath(names[0]).exists()
+            assert not (await root.joinpath(names[1]).exists())
+            assert await root.joinpath(names[2]).exists()
+            await anyio.sleep(1)
+            assert await root.joinpath(names[0]).exists()
+            assert await root.joinpath(names[1]).exists()
+            assert (await root.joinpath(names[2]).read_text()) > str(now)
+            yield
+        now = datetime.now()
+        assert (await root.joinpath(names[2]).read_text()) <= str(now)
+        await remove_files()
+
+    async with lifespan(None):
+        assert await root.joinpath(names[0]).exists()
+        assert await root.joinpath(names[1]).exists()
+        assert await root.joinpath(names[2]).exists()
+
+    assert not await root.joinpath(names[0]).exists()
+    assert not await root.joinpath(names[1]).exists()
+    assert not await root.joinpath(names[2]).exists()
