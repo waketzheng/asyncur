294f99f1860230697a2d88c78cd317c65b055cb8
Waket Zheng <waketzheng@gmail.com>
2023-10-11 22:41:32 +0800

    chore: expose __version__

--- asyncur/__init__.py
+++ asyncur/__init__.py
@@ -1,4 +1,14 @@
-from asyncur.aio import gather, run_async, start_tasks
-from asyncur.xls import load_xls, read_excel
+import importlib.metadata
 
-__all__ = ("run_async", "gather", "start_tasks", "load_xls", "read_excel")
+from asyncur.aio import gather, run_async, start_tasks
+from asyncur.xls import Path, load_xls, read_excel
+
+__version__ = importlib.metadata.version(Path(__file__).parent.name)
+__all__ = (
+    "__version__",
+    "run_async",
+    "gather",
+    "start_tasks",
+    "load_xls",
+    "read_excel",
+)
